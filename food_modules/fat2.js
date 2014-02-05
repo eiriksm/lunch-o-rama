d388602d2797a30c59c4d42717c70f5af9c06995
Eirik S. Morland <eirik@morland.no>
2014-02-05 15:45:15 +0100

    Add another food module.

---
+++ food_modules/fat2.js
@@ -0,0 +1,43 @@
+'use strict';
+var request = require('request'),
+  cheerio = require('cheerio');
+
+var urls = [
+  'http://thatiswhyyourefat.tumblr.com'
+];
+
+var i = 2;
+while (i < 65) {
+  urls.push('http://thatiswhyyourefat.tumblr.com/page/' + i);
+  i++;
+}
+
+var randomUrl = function() {
+  return urls[Math.floor(Math.random() * (urls.length))];
+};
+
+var doRequest = function(url, callback) {
+  request({url: url}, function(error, response, body) {
+    var $ = cheerio.load(body);
+    var imgs = $('.post .media img');
+    if (!imgs) {
+      callback('none found');
+      return;
+    }
+
+    // Find a random one of these images.
+    var img = imgs[Math.floor(Math.random() * imgs.length)];
+    if (!img || !img.attribs.src) {
+      callback('error');
+      return;
+    }
+
+    // Compose some text to use.
+    var text = (img.attribs.alt ? img.attribs.alt : '') + '<img src="' + img.attribs.src + '" />';
+    callback(null, text, "http://thatiswhyyourefat.tumblr.com/");
+  });
+};
+
+module.exports = function(callback) {
+  doRequest(randomUrl(), callback);
+};
