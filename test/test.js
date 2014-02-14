c4e73604734e1b32361e925bde4806d809846b80
Eirik S. Morland <eirik@morland.no>
2014-02-14 09:34:45 +0100

    Add another simple test for food modules.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 var assert = require('assert');
 var should = require('should');
 var fmodules = require('../food_modules');
+var _ = require('underscore');
 
 describe('food module export', function() {
   'use strict';
@@ -11,5 +12,22 @@ describe('food module export', function() {
       fmodules[mod].should.be.an.instanceOf(Function);
     }
     done();
+  });
+
+  it('Should call the callback with all modules', function(done) {
+    this.timeout(10000);
+    var i = 0;
+    for (var mod in fmodules) {
+      fmodules[mod](function(err, res) {
+        i++;
+      });
+    }
+    var waitForFinish = function() {
+      if (i >= _.size(fmodules)) {
+        done();
+      }
+      setTimeout(waitForFinish, 1000);
+    };
+    waitForFinish();
   });
 });
