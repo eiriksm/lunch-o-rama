6b42e52cb7d3de96cce329331039ce28285d25da
Eirik S. Morland <eirik@morland.no>
2014-02-14 09:57:06 +0100

    Redo some structure, add some more tests.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	@NODE_ENV=test node ./node_modules/mocha/bin/_mocha 
+	@NODE_ENV=test node ./node_modules/mocha/bin/_mocha --reporter list
 
 test-cov:
 	@NODE_ENV=test node ./node_modules/istanbul/lib/cli.js \
--- test/test.js
+++ test/test.js
@@ -3,9 +3,11 @@
 var assert = require('assert');
 var should = require('should');
 var fmodules = require('../food_modules');
+var Lunchword = require('../lib/lunchwords');
+var Message = require('../lib/message');
 var _ = require('underscore');
 
-describe('food module export', function() {
+describe('Food modules export', function() {
   'use strict';
   it('Should export a function for each food module', function(done) {
     for (var mod in fmodules) {
@@ -29,5 +31,39 @@ describe('food module export', function() {
       setTimeout(waitForFinish, 1000);
     };
     waitForFinish();
+  });
+});
+
+describe('Lunchwords', function() {
+  it('Should be exposed as a function', function(done){
+    Lunchword.should.be.an.instanceOf(Function);
+    done();
+  });
+
+  it('Should contain a word when we "new" it', function() {
+    var word = new Lunchword();
+    word.word.should.be.an.instanceOf(String);
+  });
+});
+
+describe('Message', function() {
+  it('Should be exposed as a function', function() {
+    Message.should.be.an.instanceOf(Function);
+  });
+
+  it('Should be possible to "new" and create a form', function() {
+    var post = new Message('test text');
+    post.form.message.should.equal('test text');
+  });
+
+  it('Should be created as html when specified', function() {
+    var post = new Message('test text', true);
+    post.form.message_format.should.equal('html');
+  });
+
+  it('Should do something when we try to post it', function() {
+    var post = new Message('test text', true);
+    post.post();
+    post.error.should.equal(true);
   });
 });
