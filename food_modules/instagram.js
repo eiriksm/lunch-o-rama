dab914796cfdde6f6082ac094fa8aafbc972c6f3
Anders <anders.2205@gmail.com>
2014-02-17 13:42:13 +0100

    Improve instagram code.

--- food_modules/instagram.js
+++ food_modules/instagram.js
@@ -4,31 +4,39 @@
 
 
 Instagram = require('instagram-node-lib');
-
-Instagram.set('client_id', 'cd6adce6dde646c9bff459ab987294b6');
-Instagram.set('client_secret', '1390a042c8b04e97be7f3e8c040fb823');
+var config;
+try {
+  config = require('../config');
+}
+catch (err) {
+  config = {};
+}
 
 var instaImage = {};
 
-Instagram.tags.recent({
-  name: 'foodporn',
-  complete: function(data){
-    var pic = Math.floor(Math.random() * data.length);
-
-    instaImage.url = data[pic].images.standard_resolution.url;
-    instaImage.text = data[pic].caption.text;
-    instaImage.source = data[pic].link;
-  },
-  error: function(errorMessage, errorObject, caller) {
-    callback(error);
+function getInstagrams(callback) {
+  if (!config.instaID || !config.instaSecret) {
+    callback('Instagram credentials not provided.');
   }
-});
+  Instagram.set('client_id', config.instaID);
+  Instagram.set('client_secret', config.instaSecret);
+
+  Instagram.tags.recent({
+    name: 'foodporn',
+    complete: function(data){
+      var pic = Math.floor(Math.random() * data.length);
+  
+      instaImage.url = data[pic].images.standard_resolution.url;
+      instaImage.text = data[pic].caption.text;
+      instaImage.source = data[pic].link;
+      callback(null, instaImage.text + ' <img src="' + instaImage.url + '" />', instaImage.source);
+    },
+    error: function(errorMessage, errorObject, caller) {
+      callback(errorMessage);
+    }
+  });
+}
 
 module.exports = function(callback) {
-  try {
-    callback(null, instaImage.text + ' <img src="' + instaImage.url + '" />', instaImage.source);
-  }
-  catch(err) {
-    callback(error);
-  }
+  getInstagrams(callback);
 }
